c3bcdd223518af8b237df006b734894b72f455db
shawky_53 <mostafashawky100000@gmail.com>
2024-12-18 22:21:25 +0200

    feat: add initial HTML, CSS, and JavaScript files for challenge setup

--- Advanced JavaScript/Advanced Flow Control/promises.js
+++ Advanced JavaScript/Advanced Flow Control/promises.js
@@ -71,17 +71,17 @@
 // asyncFunction();
 
 //refactor the above code using async and await with a for loop
-const delay = (ms) => new Promise((resolve, reject) => setTimeout(resolve, ms));
+// const delay = (ms) => new Promise((resolve, reject) => setTimeout(resolve, ms));
 
-try {
-  const asyncFunction = async () => {
-    for (let i = 2; i <= 8; i++) {
-      await delay(i * 1000);
-      console.log(`${i} seconds`);
-    }
-  };
+// try {
+//   const asyncFunction = async () => {
+//     for (let i = 2; i <= 8; i++) {
+//       await delay(i * 1000);
+//       console.log(`${i} seconds`);
+//     }
+//   };
 
-  asyncFunction();
-} catch (error) {
-  console.log(error);
-}
+//   asyncFunction();
+// } catch (error) {
+//   console.log(error);
+// }
