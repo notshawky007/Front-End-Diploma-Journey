5ee1f0535369aa86060b5a732232cceaa2c773db
shawky_53 <mostafashawky100000@gmail.com>
2025-01-07 14:23:14 +0200

    feat: add CalorieRecord component to display calorie entries in App

--- react/reactBasics/calorie-traker/src/App.jsx
+++ react/reactBasics/calorie-traker/src/App.jsx
@@ -1,8 +1,11 @@
 import "./App.css";
+import CalorieRecord from "./components/CalorieRecord";
+
 function App() {
   return (
     <>
       <h1>Calorie Tracker</h1>
+      <CalorieRecord />
     </>
   );
 }
---
+++ react/reactBasics/calorie-traker/src/components/CalorieRecord.jsx
@@ -0,0 +1,12 @@
+function CalorieRecord() {
+  return (
+    <ui>
+      <li> 400 Calorie </li>
+      <li> BreakFast </li>
+      <li> Eggs + Beans </li>
+      <li> 7/1/2025 </li>
+    </ui>
+  );
+}
+
+export default CalorieRecord;
