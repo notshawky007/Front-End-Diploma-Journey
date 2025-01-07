a08fb4b3523fe97390a6b72b6334cf4392385b6f
shawky_53 <mostafashawky100000@gmail.com>
2025-01-07 15:00:33 +0200

    feat: refactor CalorieRecord component with updated CSS classes and dynamic data display

--- react/reactBasics/calorie-traker/src/components/CalorieRecord.jsx
+++ react/reactBasics/calorie-traker/src/components/CalorieRecord.jsx
@@ -1,12 +1,18 @@
 import "./CalorieRecord.css";
 
 function CalorieRecord() {
+  const recordData = new Date().toLocaleDateString();
+  const mealType = "Breakfast";
+  const content = "Eggs and Beans on Toast";
+  const calorieCount = 350;
+  const calorieGoal = 500;
   return (
-    <ul className="record">
-      <li> Jan 7, 2025</li>
-      <li> Breakfast </li>
-      <li> Eggs + Beans </li>
-      <li className="record-calories"> 400 calories </li>
+    <ul className="calorie-record">
+      <li className="record-data">{recordData}</li>
+      <li className="meal-type">{mealType}</li>
+      <li className="content">{content}</li>
+      <li className="calorie-count">{calorieCount} calories</li>
+      <li className="calorie-goal">Goal: {calorieGoal} calories</li>
     </ul>
   );
 }
